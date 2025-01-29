78a854b27c68db671619412a75b6c0d231376199
ignas88 <ignas.c88@gmail.com>
2025-01-29 18:42:44 +0200

    use pexels api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const fetchDataForPosts = async () => {
+      try {
+        const response = await fetch('https://api.pexels.com/v1/search?query=nature&page=1', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: import.meta.env.VITE_API_KEY,
+          },
+        });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const postsData = await response.json();
+        console.log(postsData)
+      } catch (err) {
+        console.log(err)
+        console.log('err')
+      } finally {
+        console.log('finally')
+      }
+    };
+
+    fetchDataForPosts();
+  }, []);
 
   return (
     <>
