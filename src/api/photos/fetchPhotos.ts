82cbc675e16f63010c8ed3e3ba9c7579e16aa7a2
ignas88 <ignas.c88@gmail.com>
2025-01-31 11:54:01 +0200

    lint2

--- src/api/photos/fetchPhotos.ts
+++ src/api/photos/fetchPhotos.ts
@@ -8,7 +8,11 @@ const options = {
     Authorization: import.meta.env.VITE_API_KEY,
   }
 };
-export const fetchPhotos = async ({query = 'nature', page = 1, per_page = 15}: Params): Promise<Photo[]> => {
+export const fetchPhotos = async ({
+  query = 'nature',
+  page = 1,
+  per_page = 15,
+}: Params): Promise<Photo[]> => {
   const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${page}&per_page=${per_page}&orientation=landscape`, options);
   if (!response.ok) {
     throw new Error(response.statusText);
