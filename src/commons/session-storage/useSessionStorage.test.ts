298da0c97c7adb85ea85d551f8da54289fea5ab9
ignas88 <ignas.c88@gmail.com>
2025-02-01 21:18:32 +0200

    storage test

---
+++ src/commons/session-storage/useSessionStorage.test.ts
@@ -0,0 +1,50 @@
+import {renderHook, act} from '@testing-library/react';
+import {useSessionStorage} from './useSessionStorage';
+
+
+export const mockStorage = (): void => {
+  class StorageMock implements Omit<Storage, 'key' | 'length'> {
+    store: Record<string, string> = {}
+
+    clear() {
+      this.store = {}
+    }
+
+    getItem(key: string) {
+      return this.store[key] || null
+    }
+
+    setItem(key: string, value: unknown) {
+      this.store[key] = value + ''
+    }
+  }
+
+  Object.defineProperty(window, 'sessionStorage', {
+    value: new StorageMock(),
+  })
+}
+mockStorage();
+
+describe('useSessionStorage()', () => {
+  beforeEach(() => {
+    window.sessionStorage.clear()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should accept initial state as an array', () => {
+    const {result} = renderHook(() => useSessionStorage('test', [1, 2]))
+    expect(result.current[0]).toEqual([1, 2])
+  });
+  it('setValue should store now state in window.sessionStorage as serialised JSON', () => {
+    const { result } = renderHook(() => useSessionStorage('test', [1, 2]))
+
+    act(() => {
+      const setValue = result.current[1]
+      setValue([1, 2, 3])
+    })
+    expect(window.sessionStorage.getItem('test')).toBe(JSON.stringify([1, 2, 3]))
+  });
+})
